10321bf2ee04a61f31bf72f0ed0bd5f3dde93eb5
ryanlopezzzz <ryanlopez@ucsb.edu>
2021-06-02 16:28:29 -0700

    Improved and updated plots of filter gains

--- testing_circuit/plot_filter_data.py
+++ testing_circuit/plot_filter_data.py
@@ -3,13 +3,15 @@ PROGRAM DESCRIPTION:
 
 We use this program to graph pickled filter gain data in voltage gain and decibel units.
 """
-
+import os
 import pickle
 import numpy as np
 import collections
+import matplotlib
 import matplotlib.pyplot as plt
 
 filename = input('Please enter filename path to read: ')
+filename = os.path.join('pickle_data', filename)+'.pickle'
 file = open(filename, 'rb')
 data = pickle.load(file)
 file.close()
@@ -40,13 +42,24 @@ else:
     ax2 = fig2.add_axes((0.1,0.2,0.8,0.7))
     fig2.text(.5, 0.02, caption, ha='center', fontsize=12, wrap=True)
     
-ax1.errorbar(frequencies, gains, yerr = stderrs, fmt='bo')
-ax1.set(xlabel='Frequency (Hz)', ylabel='Voltage Gain', title=title)    
+ax1.errorbar(frequencies, gains, yerr = stderrs, fmt='bo', capsize=5, ms=5)
+ax1.axvspan(8, 12, alpha=0.25, label='Alpha Wave Range', linestyle='--', color='purple')
+ax1.set(xlabel='Frequency (Hz)', ylabel='Voltage Gain', title=title)
+ax1.set_xscale('log')
+ax1.set_xticks([2,4,6,8,12,20,40,60,100])
+ax1.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+ax1.legend()
 
 gains_db = 20*np.log10(gains) #gains in decibel units
 stderrs_db = (20/np.log(10)) * np.divide(stderrs, gains) #get differential error
-ax2.errorbar(frequencies, gains_db, yerr = stderrs_db, fmt='bo')
-ax2.set(xlabel='Frequency (Hz)', ylabel='Gain (dB)', title=title)    
+ax2.errorbar(frequencies, gains_db, yerr = stderrs_db, fmt='bo', capsize=5, ms=5)
+ax2.axvspan(8, 12, alpha=0.25, label='Alpha Wave Range', linestyle='--', color='purple')
+ax2.set(xlabel='Frequency (Hz)', ylabel='Gain (dB)', title=title)
+ax2.set_xscale('log')
+ax2.set_xticks([2,4,6,8,12,20,40,60,100])
+ax2.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+ax2.legend()
+
 plt.show()
 
 input('Press <Enter> to end')
