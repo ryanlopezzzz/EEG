c88a55ff00595f2252dbcf7ee547401d3832bd45
Ryan Lopez <ryanlopez@ucsb.edu>
2022-05-23 00:30:44 -0700

    Changed ADC package used
    Old code used package not available to public. 
    
    Co-authored-by: Aryan Wagh <aryanwagh98@gmail.com>

--- basic_use_eeg.py
+++ basic_use_eeg.py
@@ -16,16 +16,25 @@ import sys
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from Adafruit import ADS1x15
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.ads1x15 import Mode
+from adafruit_ads1x15.analog_in import AnalogIn
+import board
+import busio
 from analysis_tools import get_power_spectrum, get_rms_voltage, get_brain_wave, gaussian_eval, get_cutoff
+
+i2c = busio.I2C(board.SCL, board.SDA, frequency=1000000)
 
 indata = np.zeros(nsamples, 'float')
 print()
 print('Initializing ADC...')
 print()
 
-adc = ADS1x15()
-adc.startContinuousDifferentialConversion(2, 3, pga=VRANGE, sps=SPS)
+ads = ADS.ADS1115(i2c)
+chan = AnalogIn(ads, ADS.P0, ADS.P1) # This puts ADC in differential mode which reads the voltage difference between pin 0 and pin 1
+ads.mode = Mode.CONTINUOUS #This puts ADC in continuous mode
+ads.data_rate = 860 # Change actual value of data rate
+data1 = chan.voltage
 
 input('Press <Enter> to start %.1f s data acquisition...' % ACQTIME)
 print()
@@ -33,13 +42,12 @@ print()
 t0 = time.perf_counter()
 for i in range(nsamples): #loops and records data
     st = time.perf_counter()
-    time_series[i] = 0.001*adc.getLastConversionResults() #Gets voltage in volts
-    time_series[i] -= 3.3 #adc ground is 3.3 volts below circuit ground 
+    indata[i] = 0.001*adc.getLastConversionResults() #Gets voltage in volts
+    indata[i] -= 3.3 #adc ground is 3.3 volts below circuit ground 
     while (time.perf_counter() - st) <= sinterval:
             pass
 
 t = time.perf_counter() - t0
-adc.stopContinuousConversion()
 
 xpoints = np.arange(0, ACQTIME, sinterval)
 
@@ -48,12 +56,12 @@ print()
 
 #Plots raw input data to the ADC
 f1, ax1 = plt.subplots()
-ax1.plot(xpoints, time_series)
+ax1.plot(xpoints, indata)
 ax1.set(xlabel='Time (s)', ylabel='Voltage', title='Raw Signal')    
 f1.show()
 
 #Plots power spectrum of data (not normalized)
-ps = get_power_spectrum(time_series)
+ps = get_power_spectrum(indata)
 freq = np.fft.fftfreq(nsamples, d=1.0/SPS) #frequencies for FFT of data
 f2, ax2 = plt.subplots()
 ax2.plot(freq, ps)
@@ -61,7 +69,7 @@ ax2.set(xlabel='Frequency (Hz)', ylabel='Power', title='Power Spectrum')
 f2.show()
 
 #Plots brain wave (raw data with frequencies outside 8 to 12 Hz filtered out)
-brain_wave = get_brain_wave(time_series, freq_min, freq_max, freq)
+brain_wave = get_brain_wave(indata, freq_min, freq_max, freq)
 f3, ax3 = plt.subplots()
 ax3.plot(xpoints, brain_wave)
 ax3.set(xlabel='Time (s)', ylabel='Voltage', title='Alpha Wave')    
